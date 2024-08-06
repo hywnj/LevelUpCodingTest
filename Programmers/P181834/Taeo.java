0d595746cd4c240336de74e1070609d9a829d63f
metaeo <hantive98@gmail.com>
2024-08-06 11:51:34 +0900

    commit-2

--- Programmers/P181834/Taeo.java
+++ Programmers/P181834/Taeo.java
@@ -3,11 +3,11 @@ package Programmers.P181834;
 public class Taeo {
     public String solution(String myString) {
         StringBuilder answer = new StringBuilder(myString);
-        for (int i =0 ; i < myString.length(); i++) {
+        for (int i = 0; i < myString.length(); i++) {
             if (answer.toString().charAt(i) < 'l') {
                 answer.setCharAt(i, 'l');
             }
         }
         return answer.toString();
-}
+    }
 }
