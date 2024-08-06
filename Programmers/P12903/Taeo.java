98afad7f9d1d72d305844c6e445408ff6713be68
AN TAEO <70478379+metaeo@users.noreply.github.com>
2024-08-06 09:41:50 +0900

    Add files via upload

---
+++ Programmers/P12903/Taeo.java
@@ -0,0 +1,18 @@
+class Taeo {
+    public String solution(String s) {
+        String answer = "";
+
+        if(s.length() % 2 == 0){
+
+            answer += s.charAt(s.length()/2-1);
+            answer += s.charAt(s.length()/2);
+        }
+
+        else {
+
+            answer += s.charAt(s.length()/2);
+        }
+
+        return answer;
+    }
+}
