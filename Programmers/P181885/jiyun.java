2984756b1aee95690492908cb490d0d9304a9f13
koreanpaper <han010612@naver.com>
2024-08-14 12:47:53 +0900

    jiyun commit

---
+++ Programmers/P181885/jiyun.java
@@ -0,0 +1,19 @@
+package Programmers.P181885;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class jiyun {
+    public List solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (finished[i] == false) {
+                answer.add(todo_list[i]);
+            }
+        }
+
+        return answer;
+    }
+}
+
+
