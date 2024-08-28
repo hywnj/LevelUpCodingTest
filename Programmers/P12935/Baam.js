4f7b92a9867ef0f62c39deb0816973c0bcbf65fe
김범수 <qjatn1236@naver.com>
2024-08-29 08:28:30 +0900

    add Baams

---
+++ Programmers/P12935/Baam.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+  var answer = [];
+
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  arr.length <= 1 ? (answer = [-1]) : (answer = arr);
+
+  return answer;
+}
---
+++ Programmers/P181908/Baam.js
@@ -0,0 +1,5 @@
+function solution(my_string, is_suffix) {
+  const answer = my_string.endsWith(is_suffix) ? 1 : 0;
+
+  return answer;
+}
