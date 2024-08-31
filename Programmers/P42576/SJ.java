84e6721eae6b6939925e4c0d9d1788e9b54cdc2d
sujin <sujin7048@naver.com>
2024-08-31 13:01:24 +0900

    add SJ

---
+++ Programmers/P42576/SJ.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class SJ {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for ( i=0; i<completion.length; i++){
+
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
