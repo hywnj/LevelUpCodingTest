756e21dd9ad7f588f769516f965b888dd5beec13
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-08-31 20:05:07 +0900

    Add JK (8월 보충)

---
+++ Programmers/p181945/JK.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PG181945 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i=0; i<a.length(); i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+}
